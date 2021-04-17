f85ab5b2f43c30ea13575bd23f6756ac97b839ea
Mickael Lherminez <mickael.Lherminez@gmail.com>
2021-04-17 19:32:44 +0200

    1.1: 1.Installer Vue.js

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# TODO LIST VUE3
+
+## 1.0.0
+- 1.Installer Vue.js
---
+++ main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            name: "Pizza Royale",
+            price: 12
+        }
+    }
+})
+ 
